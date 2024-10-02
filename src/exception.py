2912340f9f56f0e907ae4afe13f506087a327142
TS0713 <tsp.0713@gmail.com>
2024-10-02 23:14:10 +0530

    logger & exception .py file added

---
+++ src/exception.py
@@ -0,0 +1,31 @@
+import sys
+from logger import logging
+
+def error_message_details(error: str, error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in Python Script name[{0}] Line number [{1}] Error Message [{2}]".format(
+        file_name,exc_tb.tb_lineno,str(error)
+        )
+    return error_message
+    
+
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error=error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+
+
+
+if __name__=="__main__":
+    try:
+        a = 'a'+10
+    except Exception as e:
+        logging.info(error_message_details(e,sys))
+        raise CustomException(e,sys)
+
+
--- src/logger.py
+++ src/logger.py
@@ -1,7 +1,21 @@
-import sys
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%Y_%b_%d_%HH_%MM_%SS')}.log"
+logs_path = os.path.join(os.getcwd(),"logs")
+
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
 
 
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 
-def error_message_details(error: str, error_details:sys):
-    _,_,exc_tb = error_details.exc_info()
-    pass
+)
+
+if __name__=="__main__":
+    logging.info("Started Logging")
